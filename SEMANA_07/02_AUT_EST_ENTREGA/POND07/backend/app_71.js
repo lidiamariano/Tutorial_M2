33d2ed74d176e384f6c8ba189c3d1a66ba979039
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2023-06-03 16:57:36 -0300

    att sem07

---
+++ SEMANA_07/02_AUT_EST_ENTREGA/POND07/backend/app_71.js
@@ -0,0 +1,87 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = './backend/sql.db';
+
+const hostname = '127.0.0.1';
+const port = 3071;
+const app = express();
+
+/* Servidor aplicação */
+
+app.use(express.static("./frontend/"));
+/* Definição dos endpoints */
+
+/******** CRUD ************/
+app.use(express.json());
+
+// Retorna todos registros (é o R do CRUD - Read)
+app.get('/users', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM pessoa';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/userinsert', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "INSERT INTO pessoa (Nome, Cargo) VALUES ('" + req.body.Nome + "', '" + req.body.Cargo + "')";
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/userupdate', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	//res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "UPDATE pessoa SET Nome = '" + req.body.Nome + "' WHERE id_pessoa = " + req.body.id_pessoa;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.post('/userdelete', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "DELETE FROM pessoa WHERE id_pessoa = " + req.body.id_pessoa;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Page server running at http://${hostname}:${port}/`);
+});
---
+++ SEMANA_07/02_AUT_EST_ENTREGA/POND07/frontend/adiciona_pessoa/script.js
@@ -0,0 +1,94 @@
+﻿api = 'http://127.0.0.1:3071'
+
+$(document).ready(() => {
+    users.list();
+});
+
+var users = {
+    
+    list() {
+        $.ajax({
+            url: api + '/users',
+            type: 'GET',
+            success: data => {
+                var tx = '';
+                tx += '<div class="insert" onclick="user.insert()">Inserir pessoa em seu currículo</div>';
+                data.forEach(element => {
+                    tx += '<div class="user">';
+                        tx += '<div class="title">' + `${element.Nome} - ${element.Cargo}` + '</div>';
+                        tx += '<div class="actions">';
+                            tx += '<div class="action" onclick="user.update(' + element.id_pessoa + ',\'' + element.Nome + '\')">Editar</div>';
+                            tx += '<div class="action" onclick="user.delete(' + element.id_pessoa + ')">Excluir</div>';
+                        tx += '</div>';
+                    tx += '</div>';
+                });
+                $('#main').html(tx);
+            }
+        });
+        
+    }
+    
+};
+
+var user = {
+
+    insert() {
+        var Nome = prompt('Digite o nome:');
+        var Cargo = prompt('Digite o cargo:');
+        console.log(`${Nome} - ${Cargo}`);
+        if (Nome && Cargo) {
+            if (Nome.trim() != '' && Cargo.trim() != '') {
+                $.ajax({
+                    type: 'POST',
+                    url: api + '/userinsert',
+                    data: {Nome: Nome, Cargo: Cargo},
+                }).done(function () {
+                    users.list();
+                }).fail(function (msg) {
+                    //console.log('FAIL');
+                }).always(function (msg) {
+                    //console.log('ALWAYS');
+                });
+            }
+        }
+    },
+
+
+    update(id_pessoa, oldTitle) {
+
+        var Nome = prompt('Digite o novo nome:', oldTitle);
+        if (Nome) {
+            if (Nome.trim() != '') {
+                $.ajax({
+                    type: 'POST',
+                    url: api + '/userupdate',
+                    data: {Nome: Nome, id_pessoa: id_pessoa},
+                }).done(function () {
+                    users.list();
+                }).fail(function (msg) {
+                    //console.log('FAIL');
+                }).always(function (msg) {
+                    //console.log('ALWAYS');
+                });
+            }
+        }
+    },
+
+    delete(id_pessoa) {
+
+        if (confirm('Confirma a exclusão?')) {
+            $.ajax({
+                type: 'POST',
+                url: api + '/userdelete',
+                data: {id_pessoa: id_pessoa},
+            }).done(function () {
+                users.list();
+            }).fail(function (msg) {
+                //console.log('FAIL');
+            }).always(function (msg) {
+                //console.log('ALWAYS');
+            });
+        }
+    },
+
+}
